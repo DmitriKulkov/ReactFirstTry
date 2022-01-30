51024f3f41606aea5f012a37ec60779af56cb57d
DmitriKulkov <workmailDK1102@gmail.com>
2022-01-30 16:47:09 +0300

    Props & Lists

--- react-first-try/src/App.js
+++ react-first-try/src/App.js
@@ -2,22 +2,24 @@ import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css"
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 function App() {
-  const [value, setValue] = useState("Input Text")
+  const [posts, setPosts] = useState([
+      {id: 1, title: "Javascript", body: "Description"},
+      {id: 2, title: "Javascript1", body: "Description"},
+      {id: 3, title: "Javascript2", body: "Description"},
+  ])
+  const [posts2, setPosts2] = useState([
+      {id: 1, title: "Python", body: "Description"},
+      {id: 2, title: "Python1", body: "Description"},
+      {id: 3, title: "Python2", body: "Description"},
+  ])
 
   return (
       <div className="App">
-        <div className="post">
-          <div className="post__content">
-              <strong>1. Javascript</strong>
-              <div>
-                  Javascript - programming language
-              </div>
-              <div className="post__btns">
-                  <button>Delete</button>
-              </div>
-          </div>
-        </div>
+        <PostList posts={posts} title="Javascript Posts"/>
+        <PostList posts={posts2} title="Python Posts"/>
       </div>
   );
 }
