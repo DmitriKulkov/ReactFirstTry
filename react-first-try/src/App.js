7b2056a4e7c1814ff6188581dcea9a446238d131
DmitriKulkov <workmailDK1102@gmail.com>
2022-01-30 17:13:03 +0300

    UI Library

--- react-first-try/src/App.js
+++ react-first-try/src/App.js
@@ -4,22 +4,32 @@ import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css"
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([
       {id: 1, title: "Javascript", body: "Description"},
       {id: 2, title: "Javascript1", body: "Description"},
       {id: 3, title: "Javascript2", body: "Description"},
   ])
-  const [posts2, setPosts2] = useState([
-      {id: 1, title: "Python", body: "Description"},
-      {id: 2, title: "Python1", body: "Description"},
-      {id: 3, title: "Python2", body: "Description"},
-  ])
+    const [title, setTitle] = useState("")
+    const addNewPost = (e) => {
+        e.preventDefault()
+    }
+
 
   return (
       <div className="App">
+          <form>
+              <MyInput
+                  value={title}
+                  onChange={e=>setTitle(e.target.value)}
+                  type="text"
+                  placeholder="Posts Name"/>
+              <MyInput type="text" placeholder="Posts Description"/>
+              <MyButton onClick={addNewPost}>Add Post</MyButton>
+          </form>
         <PostList posts={posts} title="Javascript Posts"/>
-        <PostList posts={posts2} title="Python Posts"/>
       </div>
   );
 }
