7c734e4bb30ad0e63104a9ef2a60b9849bcf9384
DmitriKulkov <workmailDK1102@gmail.com>
2022-02-06 13:43:28 +0300

    Custom Hook

--- react-first-try/src/App.js
+++ react-first-try/src/App.js
@@ -12,12 +12,16 @@ import {usePosts} from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 function App() {
   const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: "", query:""})
     const [visible, setVisible] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async ()=> {
+        const posts = await PostService.getAll();
+        setPosts(posts)
+    })
 
     useEffect(()=> {
         fetchPosts()
@@ -29,16 +33,6 @@ function App() {
     }
     const removePost = (post) => {
       setPosts(posts.filter(p => p.id !== post.id))
-    }
-    async function fetchPosts()
-    {
-        setIsPostsLoading(true);
-        setTimeout(async ()=>{
-            const posts = await PostService.getAll();
-            setPosts(posts)
-            setIsPostsLoading(false);
-        }, 1000)
-
     }
 
   return (
@@ -55,13 +49,14 @@ function App() {
               filter={filter}
               setFilter={setFilter}
           />
+          {postError &&
+          <h1 style={{textAlign: "center"}}>Error: ${postError}</h1>}
           {isPostsLoading
             ?<div style={{display: "flex", justifyContent: "center", marginTop: "50px"}}>
                   <Loader/>
               </div>
             :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS Posts"/>
           }
-          {/*<PostList remove={removePost} posts={posts} title="Javascript Posts"/>*/}
       </div>
   );
 }
