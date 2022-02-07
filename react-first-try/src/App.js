bd88a43d81b5ecbfa685d125d0886409e1918e41
DmitriKulkov <workmailDK1102@gmail.com>
2022-02-07 22:27:00 +0300

    useNavigation & useParams

--- react-first-try/src/App.js
+++ react-first-try/src/App.js
@@ -1,9 +1,6 @@
 import "./styles/App.css"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
 import Navbar from "./components/UI/Navbar/Navbar";
-import Error from "./pages/Error";
 import AppRouter from "./components/AppRouter";
 
 function App() {
