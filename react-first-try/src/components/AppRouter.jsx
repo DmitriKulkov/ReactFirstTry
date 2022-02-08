feb17fbb700963ef4c8d7a4630d415808ae64a3c
DmitriKulkov <workmailDK1102@gmail.com>
2022-02-08 14:50:35 +0300

    useContext

--- react-first-try/src/components/AppRouter.jsx
+++ react-first-try/src/components/AppRouter.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
+import React, {useContext} from "react";
+import { Route, Routes } from "react-router-dom";
 import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
+import { privateRoutes, publicRoutes } from "../router/routes";
+import Login from "../pages/Login";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path='/about' element={<About/>}/>
-            <Route exact path='/posts' element={<Posts/>}/>
-            <Route exact path='/posts/:id' element={<PostIdPage/>}/>
-            <Route path='/' element={<Posts/>}/>
-            <Route path="*" element={<Error/>}/>
-        </Routes>
-    );
+    const {isAuth, isLoading} = useContext(AuthContext);
+
+    if(isLoading){
+        return (
+            <Loader/>
+        )
+    }
+
+  return (
+          isAuth
+          ?
+              <Routes>
+          {privateRoutes.map((route) => (
+                  <Route key={route.path} element={route.element} path={route.path} exact={route.exact} />
+              ))}
+                  <Route path="*" element={<Error />} />
+              </Routes>
+
+          :
+              <Routes>
+              {publicRoutes.map((route) => (
+                  <Route key={route.path} element={route.element} path={route.path} exact={route.exact} />
+              ))}}
+                  <Route path="*" element={<Login />} />
+              </Routes>
+
+
+  );
 };
 
 export default AppRouter;
